f73354e2063e1182a6da36e61a56053165158709
Taiki Endo <te316e89@gmail.com>
2022-02-05 12:29:30 +0900

    Sync readme and crate-level docs

--- src/lib.rs
+++ src/lib.rs
@@ -69,15 +69,6 @@
 //! impl<T: Send> Trait for T {}
 //! impl Trait for Type {}
 //! ```
-//!
-//! ## Conditional negative impls
-//!
-//! The current behavior of this crate with conditional negative impls is the
-//! same as the unstable `negative_impls` feature, but
-//! [the unstable `negative_impls` feature plans to forbid it](https://github.com/rust-lang/rust/issues/79098).
-//!
-//! It's a great plan, but when it's implemented, this crate is unlikely to
-//! provide the same behavior due to the macro's limitation.
 
 #![doc(test(
     no_crate_inject,
