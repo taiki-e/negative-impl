ad7fe5146571f1dd1c2156194eb743f3a69e0a59
Taiki Endo <te316e89@gmail.com>
2023-10-09 20:39:46 +0900

    Move error helper macros to its own module

--- src/lib.rs
+++ src/lib.rs
@@ -101,20 +101,8 @@ impl Trait for Type {}
 #[allow(unused_extern_crates)]
 extern crate proc_macro;
 
-macro_rules! format_err {
-    ($span:expr, $msg:expr $(,)?) => {
-        syn::Error::new_spanned(&$span as &dyn quote::ToTokens, &$msg as &dyn std::fmt::Display)
-    };
-    ($span:expr, $($tt:tt)*) => {
-        format_err!($span, format!($($tt)*))
-    };
-}
-
-macro_rules! bail {
-    ($($tt:tt)*) => {
-        return Err(format_err!($($tt)*))
-    };
-}
+#[macro_use]
+mod error;
 
 use proc_macro::TokenStream;
 use proc_macro2::{Span, TokenStream as TokenStream2};
